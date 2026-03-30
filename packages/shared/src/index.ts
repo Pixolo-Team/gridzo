e42da86e4e8376a386f5d26a52ab4ad4df0e8e00
Mohdhussainsunesara <m.sunesara@somaiya.edu>
2026-03-30 13:12:16 +0530

    frontend and backend folder in a single repo

---
+++ packages/shared/src/index.ts
@@ -0,0 +1,2 @@
+export { APP_NAME } from "./constants/app-name.constant";
+export type { HealthResponseData } from "./types/health-response.data";
---
+++ packages/shared/src/types/health-response.data.ts
@@ -0,0 +1,4 @@
+export type HealthResponseData = {
+  appName: string;
+  status: "ok";
+};
