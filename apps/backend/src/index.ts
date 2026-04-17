b52edb1ca4549519c73ff3124aebd16f6233eff9
Mohdhussainsunesara <m.sunesara@somaiya.edu>
2026-04-17 19:25:01 +0530

    url change in backend

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -22,7 +22,7 @@ const CORS_ALLOWED_ORIGINS = (
   [
     "https://gridzo.tech",
     "https://www.gridzo.tech",
-    "https://gridzo.pixoltechnologies.com",
+    "https://gridzo.pixolotechnologies.com",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
   ].join(",")
